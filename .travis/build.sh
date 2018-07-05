fd783d278238ae4392e1de17eead815db6025554
Markus Winter <markus@wntr.me>
2018-07-05 12:52:02 +0200

    Updates build script

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+
     export PATH="$HOME/.swiftenv/bin:$HOME/.swiftenv/shims:$PATH";
     swift build; swift build -c release; swift test;
-else
+
+else # iOS | watchOS | tvOS | macOS
+
     # Prepare
-    set -o pipefail;
     xcodebuild -version;
     xcodebuild -showsdks;
 
     # Build Framework in Debug and Run Tests if specified
     if [ $RUN_TESTS == "YES" ]; then
-      xcodebuild -project "CELLULAR.xcodeproj" -scheme "$SCHEME" -destination "$DESTINATION" -configuration Debug ONLY_ACTIVE_ARCH=NO ENABLE_TESTABILITY=YES test | xcpretty;
+        xcodebuild -project "CELLULAR.xcodeproj" -scheme "$SCHEME" -destination "$DESTINATION" -configuration Debug ONLY_ACTIVE_ARCH=NO ENABLE_TESTABILITY=YES test | xcpretty;
     else
-      xcodebuild -project "CELLULAR.xcodeproj" -scheme "$SCHEME" -destination "$DESTINATION" -configuration Debug ONLY_ACTIVE_ARCH=NO build | xcpretty;
+        xcodebuild -project "CELLULAR.xcodeproj" -scheme "$SCHEME" -destination "$DESTINATION" -configuration Debug ONLY_ACTIVE_ARCH=NO build | xcpretty;
     fi
     # Build Framework in Release and Run Tests if specified
     if [ $RUN_TESTS == "YES" ]; then
-      xcodebuild -project "CELLULAR.xcodeproj" -scheme "$SCHEME" -destination "$DESTINATION" -configuration Release ONLY_ACTIVE_ARCH=NO ENABLE_TESTABILITY=YES test | xcpretty;
+        xcodebuild -project "CELLULAR.xcodeproj" -scheme "$SCHEME" -destination "$DESTINATION" -configuration Release ONLY_ACTIVE_ARCH=NO ENABLE_TESTABILITY=YES test | xcpretty;
     else
-      xcodebuild -project "CELLULAR.xcodeproj" -scheme "$SCHEME" -destination "$DESTINATION" -configuration Release ONLY_ACTIVE_ARCH=NO build | xcpretty;
+        xcodebuild -project "CELLULAR.xcodeproj" -scheme "$SCHEME" -destination "$DESTINATION" -configuration Release ONLY_ACTIVE_ARCH=NO build | xcpretty;
     fi
     # Run `pod lib lint` if specified
     if [ $POD_LINT == "YES" ]; then
-      pod lib lint;
+        pod lib lint;
     fi
 fi
