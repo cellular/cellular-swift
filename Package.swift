1b570357dbdb5a5a0a1ae815cf019c30d1918964
Markus Winter <markus@wntr.me>
2019-08-27 15:50:51 +0200

    Merged sub-modules into one CELLULAR module

--- Package.swift
+++ Package.swift
@@ -4,12 +4,10 @@ import PackageDescription
 let package = Package(
     name: "CELLULAR",
     products: [
-        .library(name: "CELLULAR", targets: ["Codable", "Locking", "Storyboard"])
+        .library(name: "CELLULAR", targets: ["CELLULAR"])
     ],
     targets: [
-        .target(name: "Codable"),
-        .target(name: "Locking"),
-        .target(name: "Storyboard"),
-        .testTarget(name: "CELLULARTests", dependencies: ["Codable", "Locking", "Storyboard"])
+        .target(name: "CELLULAR"),
+        .testTarget(name: "CELLULARTests", dependencies: ["CELLULAR"])
     ]
 )
